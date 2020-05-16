5525b14eb0d9616a931125a5a576d9963ab61e70
Eziz2 <eziz_kurbannazarow@mail.ru>
2020-05-16 20:26:08 +0300

    Это 2 ая практика была

--- index.js
+++ index.js
@@ -1,16 +1,36 @@
 const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
 const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false,
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
 };
 
-const raz1 = prompt("Один из последных просмотренных фильмов?", ""),
-    raz2 = prompt("На сколько оцените его?", ""),
-    raz1_2 = prompt("Один из последных просмотренных фильмов?", ""),
-    raz2_2 = prompt("На сколько оцените его?", "");
+if (personalMovieDB.count < 10) {
+  alert("Просмотрено довольно мало фильмов!");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+  alert("Вы классический зритель!");
+} else if (personalMovieDB.count > 30) {
+  alert("Вы киноман!");
+} else {
+  alert("Произошла ошибка!");
+}
 
-personalMovieDB.movies[raz1] = raz2;
-personalMovieDB.movies[raz1_2] = raz2_2;
+for (let i = 0; i < 2; i++) {
+  const a = prompt("Один из последных просмотренных фильмов?", "");
+  const b = prompt("На сколько оцените его?", "");
+  if (
+    a != "" &&
+    a != " " &&
+    a != null &&
+    a.length < 50 &&
+    b != "" &&
+    b != " " &&
+    b != null
+  ) {
+    personalMovieDB.movies[a] = b;
+  } else {
+    i--;
+  }
+}
